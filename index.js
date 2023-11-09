5cd25e1e9a41ee5f04aa3769bfdc646251905f1d
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-11-09 13:23:41 +0600

    finally fix all issue

--- index.js
+++ index.js
@@ -73,6 +73,8 @@ async function run() {
       res
         .clearCookie("token", {
           maxAge: 0,
+          secure: process.env.NODE_ENV === "production" ? true : false,
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
         .send({ success: true });
     });
